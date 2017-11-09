ed23b790a9d892e712dbe5786c153769ea53226e
Ageel <ashatry@gmail.com>
2017-11-09 14:13:44 -0600

    Added JS logic for the form calculation.

---
+++ calc.js
@@ -0,0 +1,8 @@
+function calc(){
+	var l = document.getElementById("length").value;
+	var w = document.getElementById("width").value;
+	var area = l * w;
+	document.getElementById("answer").value = area;
+
+}
+
